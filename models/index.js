ff8449553a3d25b22b0704cd4d84fc7ce6c8d34e
Quebvihn <bamedinatorres@gmail.com>
2023-11-01 18:23:35 -0400

    product tag index

--- models/index.js
+++ models/index.js
@@ -14,11 +14,11 @@ Category.hasMany(Product, {
   foreignKey: 'category_id',
 });
 // Products belongToMany Tags (through ProductTag)
-Product.belongsTo(ProductTag, {
+Product.belongsTo(Tag, {
   foreignKey: 'product_id',
 });
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsTo(ProductTag, {
+Tag.belongsTo(Product, {
   foreignKey: 'tag_id',
 });
 module.exports = {
